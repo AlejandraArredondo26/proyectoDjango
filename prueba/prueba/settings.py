29a72351a1a7b1344405d04c1dfe21eba70ac945
AlejandraArredondo26 <tinocoarredondoa@gmail.com>
2025-07-01 14:27:24 -0600

    cambios 01/07

--- prueba/prueba/settings.py
+++ prueba/prueba/settings.py
@@ -128,3 +128,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+#CKEDITOR
+CKEDITOR_CONFIGS ={
+    'default':{
+        'toolbar': 'Custom',
+        'toolbar_Custom': 
+        
+        [['Bold', 'Italic', 'Underline']]
+
+    }
+}
