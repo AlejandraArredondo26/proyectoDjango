4ca6996e392ae08a2fc9a3fdb0760907cb604a9c
AlejandraArredondo26 <tinocoarredondoa@gmail.com>
2025-07-15 14:31:10 -0600

    CAMBIODB

--- prueba/prueba/settings.py
+++ prueba/prueba/settings.py
@@ -77,12 +77,24 @@ WSGI_APPLICATION = 'prueba.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ejemplo',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306'
+    }
 }
+
 
 
 # Password validation
